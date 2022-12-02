0870a8fc605a7a0e423f0769ffee6293804384e8
DongNguyen <sukein03092003@gmail.com>
2022-12-02 21:24:44 +0700

    khoi tao du an

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function changeColor() {
+    var colorI= document.getElementsByClassName("emoji_item");
+    // colorI.style.color="green";
+    console.log(colorI);
+    console.log('aa');
+}
+document.getElementsByClassName("emoji_item").onclick=changeColor();
